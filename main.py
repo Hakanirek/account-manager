0db9cdc7211e7d2bf093c1775a3365b255dde419
Hakan <161349587+Hakanirek@users.noreply.github.com>
2025-03-19 16:06:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,43 +4,60 @@ import streamlit as st
 import pandas as pd
 import sqlite3
 import time
+import os
 
 
 # Function to set up the database
 def setup_database():
     with sqlite3.connect('profiles.db', timeout=10) as conn:
         c = conn.cursor()
+
+        # Drop tables if they exist to reset the schema only when debugging
+        # c.execute('DROP TABLE IF EXISTS transactions')
+        # c.execute('DROP TABLE IF EXISTS profiles')
+
+        # Set up new tables
         c.execute('''
         CREATE TABLE IF NOT EXISTS transactions (
             date TEXT,
             name TEXT,
-            value REAL,
-            UNIQUE(date, name, value) ON CONFLICT IGNORE
+            dolar REAL,
+            euro REAL,
+            zl REAL,
+            UNIQUE(date, name, dolar, euro, zl) ON CONFLICT IGNORE
         )
         ''')
         c.execute('''
         CREATE TABLE IF NOT EXISTS profiles (
             name TEXT PRIMARY KEY,
-            balance REAL
+            balance_dolar REAL,
+            balance_euro REAL,
+            balance_zl REAL
         )
         ''')
         conn.commit()
 
 
-def insert_transaction(date, name, value):
+def insert_transaction(date, name, dolar, euro, zl):
     retry_count = 5
     while retry_count > 0:
         try:
             with sqlite3.connect('profiles.db', timeout=10) as conn:
                 c = conn.cursor()
-                c.execute('INSERT INTO transactions (date, name, value) VALUES (?, ?, ?)', (date, name, value))
-                c.execute('SELECT balance FROM profiles WHERE name = ?', (name,))
+                c.execute('INSERT INTO transactions (date, name, dolar, euro, zl) VALUES (?, ?, ?, ?, ?)',
+                          (date, name, dolar, euro, zl))
+                c.execute('SELECT balance_dolar, balance_euro, balance_zl FROM profiles WHERE name = ?', (name,))
                 result = c.fetchone()
                 if result:
-                    new_balance = result[0] + value
-                    c.execute('UPDATE profiles SET balance = ? WHERE name = ?', (new_balance, name))
+                    new_balance_dolar = result[0] + dolar
+                    new_balance_euro = result[1] + euro
+                    new_balance_zl = result[2] + zl
+                    c.execute('UPDATE profiles SET balance_dolar = ?, balance_euro = ?, balance_zl = ? WHERE name = ?',
+                              (new_balance_dolar, new_balance_euro, new_balance_zl, name))
                 else:
-                    c.execute('INSERT INTO profiles (name, balance) VALUES (?, ?)', (name, value))
+                    c.execute(
+                        'INSERT INTO profiles (name, balance_dolar, balance_euro, balance_zl) VALUES (?, ?, ?, ?)',
+                        (name, dolar, euro, zl))
                 conn.commit()
             break
         except sqlite3.OperationalError as e:
@@ -65,14 +82,15 @@ def fetch_transactions(date, profile, all_dates):
         c = conn.cursor()
         if all_dates:
             if profile == 'All Profiles':
-                c.execute('SELECT date, name, value FROM transactions')
+                c.execute('SELECT date, name, dolar, euro, zl FROM transactions')
             else:
-                c.execute('SELECT date, name, value FROM transactions WHERE name = ?', (profile,))
+                c.execute('SELECT date, name, dolar, euro, zl FROM transactions WHERE name = ?', (profile,))
         else:
             if profile == 'All Profiles':
-                c.execute('SELECT date, name, value FROM transactions WHERE date = ?', (date,))
+                c.execute('SELECT date, name, dolar, euro, zl FROM transactions WHERE date = ?', (date,))
             else:
-                c.execute('SELECT date, name, value FROM transactions WHERE date = ? AND name = ?', (date, profile))
+                c.execute('SELECT date, name, dolar, euro, zl FROM transactions WHERE date = ? AND name = ?',
+                          (date, profile))
         transactions = c.fetchall()
     return transactions
 
@@ -81,11 +99,12 @@ def fetch_monthly_summary(month, profile):
     with sqlite3.connect('profiles.db', timeout=10) as conn:
         c = conn.cursor()
         if profile == 'All Profiles':
-            c.execute('SELECT name, SUM(value) FROM transactions WHERE strftime("%m", date) = ? GROUP BY name',
-                      (month,))
+            c.execute(
+                'SELECT name, SUM(dolar) as total_dolar, SUM(euro) as total_euro, SUM(zl) as total_zl FROM transactions WHERE strftime("%m", date) = ? GROUP BY name',
+                (month,))
         else:
             c.execute(
-                'SELECT name, SUM(value) FROM transactions WHERE strftime("%m", date) = ? AND name = ? GROUP BY name',
+                'SELECT name, SUM(dolar) as total_dolar, SUM(euro) as total_euro, SUM(zl) as total_zl FROM transactions WHERE strftime("%m", date) = ? AND name = ? GROUP BY name',
                 (month, profile))
         monthly_summary = c.fetchall()
     return monthly_summary
@@ -95,93 +114,87 @@ def fetch_yearly_summary(year, profile):
     with sqlite3.connect('profiles.db', timeout=10) as conn:
         c = conn.cursor()
         if profile == 'All Profiles':
-            c.execute('SELECT name, SUM(value) FROM transactions WHERE strftime("%Y", date) = ? GROUP BY name', (year,))
+            c.execute(
+                'SELECT name, SUM(dolar) as total_dolar, SUM(euro) as total_euro, SUM(zl) as total_zl FROM transactions WHERE strftime("%Y", date) = ? GROUP BY name',
+                (year,))
         else:
             c.execute(
-                'SELECT name, SUM(value) FROM transactions WHERE strftime("%Y", date) = ? AND name = ? GROUP BY name',
+                'SELECT name, SUM(dolar) as total_dolar, SUM(euro) as total_euro, SUM(zl) as total_zl FROM transactions WHERE strftime("%Y", date) = ? AND name = ? GROUP BY name',
                 (year, profile))
         yearly_summary = c.fetchall()
     return yearly_summary
 
 
 def run_streamlit():
-    # Open Streamlit app only once
-    if len(sys.argv) == 1:  # Check if script is called without additional arguments
+    if len(sys.argv) == 1:
         subprocess.run([sys.executable, "-m", "streamlit", "run", sys.argv[0], '--server.port=8501'])
 
 
-# Main logic to run Streamlit server only once
 if __name__ == '__main__':
-    setup_database()  # Set up database if necessary
-    run_streamlit()  # Start Streamlit in subprocess
+    setup_database()
+    run_streamlit()
 
-# Streamlit app title
 st.title("Accounting Program")
 
-# File uploader for Excel
-uploaded_file = st.file_uploader("Upload Excel file with Date, Name, Value", type="xlsx")
+uploaded_file = st.file_uploader("Upload Excel file with Date, Name, Dolar, Euro, ZL", type="xlsx")
 
 if uploaded_file:
     try:
-        # Read the uploaded Excel file
         daily_data = pd.read_excel(uploaded_file)
         daily_data['Date'] = pd.to_datetime(daily_data['Date'], format='%d.%m.%Y').dt.strftime('%Y-%m-%d')
 
-        # Insert data without checking for duplicates
         for index, row in daily_data.iterrows():
-            date, name, value = row['Date'], row['Name'], row['Value']
-            insert_transaction(date, name, value)
+            date, name = row['Date'], row['Name']
+            dolar, euro, zl = row['Dolar'], row['Euro'], row['ZL']
+            insert_transaction(date, name, dolar, euro, zl)
         st.success('Data uploaded successfully')
     except Exception as e:
         st.error(f"An error occurred: {e}")
 
-# Dropdown for profile selection
 profiles = fetch_profiles()
 selected_profile = st.selectbox("Select Profile", options=profiles)
 
-# Calendar date input for specific date selection
 st.header("Select Date for Transactions")
 selected_date = st.date_input("Choose a date", pd.to_datetime("today").date())
 
-# Button for "All Dates" selection
 all_dates = st.button("Show All Dates")
 
 if selected_profile:
     try:
-        # Fetch transactions based on whether "All Dates" is selected
         transactions = fetch_transactions(selected_date.strftime('%Y-%m-%d'), selected_profile, all_dates)
-        df_transactions = pd.DataFrame(transactions, columns=["Date", "Name", "Value"])
+        df_transactions = pd.DataFrame(transactions, columns=["Date", "Name", "Dolar", "Euro", "ZL"])
         st.subheader(
             f"Transactions for {selected_profile} on {'All Dates' if all_dates else selected_date.strftime('%d.%m.%Y')}")
         st.write(df_transactions)
     except Exception as e:
         st.error(f"An error occurred: {e}")
 
-# Monthly Summary
 st.header("Monthly Summary")
 current_month = selected_date.strftime('%m')
 try:
     monthly_summary = fetch_monthly_summary(current_month, selected_profile)
-    df_monthly_summary = pd.DataFrame(monthly_summary, columns=["Name", "Total Value"])
+    df_monthly_summary = pd.DataFrame(monthly_summary, columns=["Name", "Total Dolar", "Total Euro", "Total ZL"])
     st.write(df_monthly_summary)
 
-    # Calculate monthly total
-    monthly_total = df_monthly_summary['Total Value'].sum()
-    st.write(f"Monthly Total for {selected_profile}: {monthly_total}")
+    monthly_total_dolar = df_monthly_summary['Total Dolar'].sum()
+    monthly_total_euro = df_monthly_summary['Total Euro'].sum()
+    monthly_total_zl = df_monthly_summary['Total ZL'].sum()
+    st.write(
+        f"Monthly Total for {selected_profile}: Dolar: {monthly_total_dolar}, Euro: {monthly_total_euro}, ZL: {monthly_total_zl}")
 except Exception as e:
     st.error(f"An error occurred: {e}")
 
-# Yearly Summary
 st.header("Yearly Summary")
 current_year = selected_date.strftime('%Y')
 try:
     yearly_summary = fetch_yearly_summary(current_year, selected_profile)
-    df_yearly_summary = pd.DataFrame(yearly_summary, columns=["Name", "Total Value"])
+    df_yearly_summary = pd.DataFrame(yearly_summary, columns=["Name", "Total Dolar", "Total Euro", "Total ZL"])
     st.write(df_yearly_summary)
 
-    # Calculate yearly total
-    yearly_total = df_yearly_summary['Total Value'].sum()
-    st.write(f"Yearly Total for {selected_profile}: {yearly_total}")
+    yearly_total_dolar = df_yearly_summary['Total Dolar'].sum()
+    yearly_total_euro = df_yearly_summary['Total Euro'].sum()
+    yearly_total_zl = df_yearly_summary['Total ZL'].sum()
+    st.write(
+        f"Yearly Total for {selected_profile}--> Dolar: {yearly_total_dolar}, Euro: {yearly_total_euro}, ZL: {yearly_total_zl}")
 except Exception as e:
     st.error(f"An error occurred: {e}")
-
